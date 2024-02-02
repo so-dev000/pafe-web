c4a154d0cab1d2be065fb413b447c2c9c552bb8e
nsota-akademia <yuasoi.so@gmail.com>
2024-02-02 12:59:11 +0900

    goole and apple

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-
+import google_play_icon from "./assets/google-play-badge.png"
+import apple_store_icon from "./assets/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
 function App() {
   return (
     <>
-      <h3>Welcome to Pafe</h3>
+      <><img src={google_play_icon} style={{ width: 150 }}></img>
+        <img src={apple_store_icon}></img></>
       <Routes>
-        <Route path="/" element={< p />} />
-        <Route path="/about" element={<p />} />
-        <Route path="/contact" element={<p />} />
-        <Route path="/term" element={<p />} />
-        <Route path="/privacy-policy" element={<p />} />
-        <Route path="*" element={<p />} /> {/* not found */}
+        <Route path="/" element={<></>} />
+        <Route path="/about" element={<></>} />
+        <Route path="/contact" element={<></>} />
+        <Route path="/term" element={<></>} />
+        <Route path="/privacy-policy" element={<></>} />
+        <Route path="*" element={<></>} /> {/* not found */}
       </Routes>
     </>
   );
