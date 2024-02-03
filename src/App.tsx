4d869c7617fade90d027c17ffcef07bfa4b28e61
nsota-akademia <yuasoi.so@gmail.com>
2024-02-03 10:37:46 +0900

    header border

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ function App() {
   return (
     <>
       <>
-        <header style={{ padding: 20 }}>
+        <header style={{ padding: 20, position: 'sticky', top: 0, backgroundColor: 'white', borderBottom: "solid 2px", borderBottomColor: "#EEEEEE" }}>
           <h1 style={{ fontFamily: 'Pacifico', margin: 0, fontSize: 50, marginLeft: "20px", lineHeight: 1 }}>PAFE</h1>
         </header>
         {/* <img src={google_play_icon} style={{ width: 120 }}></img> */}
         {/* <img src={apple_store_icon}></img> */}
       </>
       <Routes>
-        <Route path="/" element={<p>landing</p>} />
+        <Route path="/" element={<h1>landing</h1>} />
         <Route path="/about" element={<p>about</p>} />
         <Route path="/term" element={<p>term</p>} />
         <Route path="/privacy-policy" element={<p>privacy-policy</p>} />
