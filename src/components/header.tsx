2822bb45cbb5c6929799ceaf21a0950b9e7e3bba
Nakamura Sota <sota_nakamura@demia.co.jp>
2024-03-15 17:19:41 +0900

    lp作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,55 +1,49 @@
 import { Link } from "react-router-dom";
 import { URLs } from "../constants/urls";
 
+import { css } from "@emotion/react";
+
 export const Header = () => {
   return (
     <>
-      <header
-        style={{
-          padding: 5,
-          position: "sticky",
-          top: 0,
-          backgroundColor: "white",
-          borderBottom: "solid 2px",
-          borderBottomColor: "#EEEEEE",
-        }}
-      >
-        <Link
-          style={{
-            fontFamily: "Pacifico",
-            margin: 0,
-            fontSize: 35,
-            marginLeft: "20px",
-            lineHeight: 1,
-            position: "absolute",
-            left: "0",
-            top: "15px",
-            textDecoration: "none",
-            color: "black",
-          }}
-          to={URLs.Lp}
-        >
+      <header css={headerStyle}>
+        <Link css={logoStyle} to={URLs.Lp}>
           PAFE
         </Link>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
-            marginLeft: "10%",
-          }}
-        >
-          <Link to={URLs.About}>
-            <p>使い方</p>
+        <div css={headerContainerStyle}>
+          <Link to={URLs.About} css={linkStyle}>
+            <p css={fontStyle}>使い方</p>
           </Link>
-          <Link to={URLs.Term}>
-            <p>利用規約</p>
+          <Link to={URLs.Term} css={linkStyle}>
+            <p css={fontStyle}>利用規約</p>
           </Link>
-          <Link to={URLs.PrivacyPolicy}>
-            <p>プライバシーポリシー</p>
+          <Link to={URLs.PrivacyPolicy} css={linkStyle}>
+            <p css={fontStyle}>プライバシーポリシー</p>
           </Link>
         </div>
       </header>
     </>
   );
 };
+
+const linkStyle = css({ textDecoration: "none" });
+const fontStyle = css({ color: "white" });
+const headerStyle = css({ padding: 5, position: "sticky", top: 0 });
+const logoStyle = css({
+  fontFamily: "Pacifico",
+  margin: 0,
+  fontSize: 35,
+  marginLeft: "20px",
+  lineHeight: 1,
+  position: "absolute",
+  left: "0",
+  top: "15px",
+  textDecoration: "none",
+  color: "white",
+});
+const headerContainerStyle = css({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around",
+  marginLeft: "10%",
+});
--- src/feature/lp/lp.tsx
+++ src/feature/lp/lp.tsx
@@ -1,3 +1,68 @@
+import { css, keyframes } from "@emotion/react";
+import ScreenShotMain from "../../assets/screenshot_main.png";
 export const Lp = () => {
-  return <p>Lp</p>;
+  return (
+    <div css={containerStyle}>
+      <img src={ScreenShotMain} css={imageStyle}></img>;
+      <h3 css={h3Style}>投票専用SNS</h3>
+      <h1 css={logoStyle}>PAFE</h1>
+    </div>
+  );
 };
+const logoAnimation = keyframes`
+0%{
+    margin-top: 7%;
+    opacity: 0;
+}
+100% {
+    margin-top: 8%;
+    opacity: 1;
+}
+`;
+const logoStyle = css({
+  fontFamily: "Pacifico",
+  fontSize: 50,
+  lineHeight: 1,
+  position: "absolute",
+  margin: "8% 30%",
+  color: "#EEEEEE",
+  animation: `${logoAnimation} 2s`,
+});
+
+const h3Animation = keyframes`
+0%{
+    margin-top: 4%;
+    opacity: 0;
+}
+100% {
+    margin-top: 5%;
+    opacity: 1;
+}
+`;
+
+const h3Style = css({
+  color: "white",
+  position: "absolute",
+  margin: "5% 30%",
+  animation: `${h3Animation} 2s`,
+});
+
+const containerStyle = css({
+  display: "flex",
+  flexDirection: "row",
+});
+
+const imageAnimation = keyframes`
+0%{
+    margin-left: 0%;
+}
+100% {
+    margin-left: 5%;
+}
+`;
+
+const imageStyle = css({
+  width: "300px",
+  margin: "5% 5%",
+  animation: `${imageAnimation} 1s`,
+});
