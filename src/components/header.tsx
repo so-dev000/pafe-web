2ab2c0675e4e69c84a8f8d56ee4bcf32514053bb
Nakamura Sota <sota_nakamura@demia.co.jp>
2024-03-16 00:19:27 +0900

    利用規約の追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { URLs } from "../constants/urls";
 
 import { css } from "@emotion/react";
 
 export const Header = () => {
+  const pathname = useLocation().pathname;
   return (
     <>
       <header css={headerStyle}>
@@ -13,12 +14,17 @@ export const Header = () => {
         <div css={headerContainerStyle}>
           <Link to={URLs.About} css={linkStyle}>
             <p css={fontStyle}>使い方</p>
+            {pathname === "/" + URLs.About && <div css={selectedStyle}></div>}
           </Link>
           <Link to={URLs.Term} css={linkStyle}>
             <p css={fontStyle}>利用規約</p>
+            {pathname === "/" + URLs.Term && <div css={selectedStyle}></div>}
           </Link>
           <Link to={URLs.PrivacyPolicy} css={linkStyle}>
             <p css={fontStyle}>プライバシーポリシー</p>
+            {pathname === "/" + URLs.PrivacyPolicy && (
+              <div css={selectedStyle}></div>
+            )}
           </Link>
         </div>
       </header>
@@ -26,9 +32,26 @@ export const Header = () => {
   );
 };
 
-const linkStyle = css({ textDecoration: "none" });
+const linkStyle = css({
+  textDecoration: "none",
+  display: "flex",
+  flexDirection: "column",
+  textAlign: "center",
+  alignItems: "center",
+  width: "200px",
+});
+const selectedStyle = css({
+  backgroundColor: "white",
+  height: "1px",
+  width: "100px",
+});
 const fontStyle = css({ color: "white" });
-const headerStyle = css({ padding: 5, position: "sticky", top: 0 });
+const headerStyle = css({
+  padding: 5,
+  position: "sticky",
+  top: 0,
+  backgroundColor: "black",
+});
 const logoStyle = css({
   fontFamily: "Pacifico",
   margin: 0,
